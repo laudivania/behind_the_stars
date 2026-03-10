405a1d4724d3dacaed1517feae1480b4dd2e5d2e
Laudivania Ventura <laudivania.ventura@hotmail.com>
2026-03-10 17:28:06 +0100

    preprocessor with vectorizing function

--- ml_logic/preprocessor.py
+++ ml_logic/preprocessor.py
@@ -2,6 +2,8 @@ import pandas as pd
 import string
 from nltk import word_tokenize
 from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 
 def preprocessing(sentence):
     sentence = sentence.strip()
@@ -18,3 +20,14 @@ def preprocessing(sentence):
     for word in token_sized_sentence]
     sentence =  " ".join(liste)
     return sentence
+
+
+
+def vectorizing(preprocessed_data):
+    """Vectorizing the data with TfidVectorizer after the preprocessing
+    in order to used it on the models"""
+    vectorizer = TfidfVectorizer()
+    vectorized_data = vectorizer.fit_transform(preprocessed_data)
+    vectorized_data = pd.DataFrame(vectorized_data.toarray(),
+                    columns = vectorizer.get_feature_names_out())
+    return vectorized_data
