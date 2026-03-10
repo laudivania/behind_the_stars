12afbb68b1c7a1e84fc7bc66a2e0eaca2dc48f58
Laudivania Ventura <laudivania.ventura@hotmail.com>
2026-03-10 17:35:32 +0100

    file with both vectorizing functions

---
+++ ml_logic/preprocessor_.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import string
+from nltk import word_tokenize
+from nltk.stem import WordNetLemmatizer
+<<<<<<< HEAD
+from sklearn.feature_extraction.text import TfidfVectorizer
+=======
+from keras.layers import TextVectorization
+>>>>>>> f79d0121d25f1383750048e7cc276b6f582115f8
+
+
+def preprocessing(sentence):
+    sentence = sentence.strip()
+    sentence = sentence.lower()
+    sentence = ''.join(char for char in sentence if not char.isdigit())
+    for punctuation in string.punctuation:
+        sentence = sentence.replace(punctuation, '')
+    token_sized_sentence = word_tokenize(sentence)
+    liste = [WordNetLemmatizer().lemmatize(word, pos = "v") # v --> verbs
+    for word in token_sized_sentence]
+    sentence =  " ".join(liste)
+    token_sized_sentence = word_tokenize(sentence)
+    liste = [WordNetLemmatizer().lemmatize(word, pos = "n") # n --> nouns
+    for word in token_sized_sentence]
+    sentence =  " ".join(liste)
+    return sentence
+
+
+
+#For Machine Learning
+def vectorizing(preprocessed_data):
+    """Vectorizing the data with TfidVectorizer after the preprocessing
+    in order to used it on the models"""
+    vectorizer = TfidfVectorizer()
+    vectorized_data = vectorizer.fit_transform(preprocessed_data)
+    vectorized_data = pd.DataFrame(vectorized_data.toarray(),
+                    columns = vectorizer.get_feature_names_out())
+    return vectorized_data
+
+
+#For Deep Learning
+def get_vectorizer(X_preproc, vocab_size=3000, output_sequence_length=None):
+    """
+
+    To vectorize, aparently ideal for Keras (Deep Learning specific)
+
+        X_preproc: The X  already cleaned strings (from our previous preprocessing).
+            # change later the preproc 'sentence' to something else TBC
+        vocab_size: 3000 to level, it seems we dont have more than that (might be optimized later)
+        output_sequence_length: padding length, I have used a np.percentile(review_lengths, 75) in my initial tests
+        (might be optimized later).
+
+    """
+
+    # Initialize
+    vectorizer = TextVectorization(
+        max_tokens=vocab_size,
+        standardize=None,
+        output_mode='int',
+        output_sequence_length=output_sequence_length
+    )
+
+    # Adapt to the dataset (it works like .fit in Keras)
+    vectorizer.adapt(X_preproc)
+
+    # Transforms the text into a np matrix
+    X_vect = vectorizer(X_preproc).numpy()
+
+    return X_vect
