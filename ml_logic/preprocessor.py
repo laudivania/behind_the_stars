aa2f5b0ec1070b615988e79c6d5d8d983d23a93c
Gautier <gvisseaux@gmail.com>
2026-03-11 14:17:38 +0100

    Update preprocessor.py
    récupération après sabotage

--- ml_logic/preprocessor.py
+++ ml_logic/preprocessor.py
@@ -1,9 +1,19 @@
 import pandas as pd
 import string
 from nltk import word_tokenize
+
+from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+
 from keras.layers import TextVectorization
+
+from nltk.stem import WordNetLemmatizer
+from langdetect import detect, DetectorFactory
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+
 #fonction customizable de prepoc
 def preprocessing(sentence, minuscule = True, ponctuation = True, stopwords = True):
     sentence = sentence.strip()
@@ -26,6 +36,53 @@ def preprocessing(sentence, minuscule = True, ponctuation = True, stopwords = Tr
     liste = [WordNetLemmatizer().lemmatize(word, pos = "n") # n --> nouns
     for word in token_sized_sentence]
     sentence =  " ".join(liste)
+    return sentence
+
+lemmatizer = WordNetLemmatizer()
+DetectorFactory.seed = 42
+def detect_language(text):
+    """
+    Detects language for a given text
+    """
+    try:
+        if pd.isna(text) or str(text).strip() == "":
+            return "Unknown language"
+        return detect(text)
+    except:
+        return "Error"
+
+def basic_cleaning(sentence:str) -> str:
+    """Removes whitespaces, converts to lowercase, strips digits
+    and remove all puntuation"""
+
+    sentence = sentence.strip().lower()
+    sentence = "".join(char for char in sentence if not char.isdigit())
+
+    for punctuation in string.punctuation:
+        sentence = sentence.replace(punctuation, "")
+    return sentence
+
+def lemmatize_verbs(sentence: str) -> str:
+    """Tokenizes the sentence and reduces verbs to their base forms"""
+
+    tokens = word_tokenize(sentence)
+    verb_lemmas = [lemmatizer.lemmatize(word, pos="v") for word in tokens]
+    return " ".join(verb_lemmas)
+
+def lemmatize_nouns(sentence: str) -> str:
+    """Tokenizes the sentence and reduces verbs to their base forms"""
+
+    tokens = word_tokenize(sentence)
+    noun_lemmas = [lemmatizer.lemmatize(word, pos="n") for word in tokens]
+    return " ".join(noun_lemmas)
+
+def full_preprocessing(sentence: str) -> str:
+    """Conbines basic_cleaning, lemmatize_verb and lemmatize_nouns into a
+    single pipeline"""
+
+    sentence = basic_cleaning(sentence)
+    sentence = lemmatize_verbs(sentence)
+    sentence = lemmatize_nouns(sentence)
     return sentence
 
 
@@ -64,9 +121,10 @@ def sum_and_sort (vectorized_data):
     return vectorized_data.sum(axis=0).sort_values(ascending=False)
 
 
-#For Deep Learning, TextVectorization
+#For Deep Learning
 def get_vectorizer(X_preproc, vocab_size=3000, output_sequence_length=None):
     """
+
     To vectorize, aparently ideal for Keras (Deep Learning specific)
 
         X_preproc: The X  already cleaned strings (from our previous preprocessing).
@@ -74,9 +132,12 @@ def get_vectorizer(X_preproc, vocab_size=3000, output_sequence_length=None):
         vocab_size: 3000 to level, it seems we dont have more than that (might be optimized later)
         output_sequence_length: padding length, I have used a np.percentile(review_lengths, 75) in my initial tests
         (might be optimized later).
+
     """
+
     # Initialize
-    vectorizer = Textvectorizer(
+    vectorizer = TextVectorization(
+
         max_tokens=vocab_size,
         standardize=None,
         output_mode='int',
