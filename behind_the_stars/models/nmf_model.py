2841ea280c64577a3cbff2817ddc9c8ad9e895be
Laudivania Ventura <laudivania.ventura@hotmail.com>
2026-03-12 11:14:50 +0100

    New package, please pull it

--- behind_the_stars/models/nmf_model.py
+++ behind_the_stars/models/nmf_model.py
@@ -1,10 +1,24 @@
-import pandas as pd
-import pyarrow.parquet as pq
-from ml_logic.preprocessor import preprocessing, vectorizing_tfid,vectorizing_countv,sum_and_sort
-from sklearn.feature_extraction.text import TfidfVectorizer
-from ml_logic.preprocessor import master_preprocessor
 from sklearn.decomposition import NMF
 from pandarallel import pandarallel
-from models import lda_topics
 
 pandarallel.initialize(progress_bar=True)
+
+
+def nmf_model(vectorized_data,num_topics random_state=None):
+    """Non-negative matrix factorization, will give us the weigh of the sentence
+    on the document and the weight of the word on the sentence"""
+    nmf_model = NMF (n_components=num_topics)
+    nmf_matrix = nmf_model.fit_transform(vectorized_data)
+    return nmf_matrix
+
+
+def print_topics_with_weights(model, vectorizer, top_n):
+    """ To print the words impacting each topic the most and
+    the associated weights"""
+    words = vectorizer.get_feature_names_out()
+    for topic_idx, topic in enumerate(model.components_):
+
+        print(f"\nTopic {topic_idx + 1}")
+        top_words = topic.argsort()[:-top_n - 1:-1]
+        for i in top_words:
+            print(words[i], round(topic[i], 3))
--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+from setuptools import find_packages
+
+with open("requirements.txt") as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if "git+" not in x]
 
 setup(name='behind_the_stars',
       version="0.1",
-      packages=find_packages())
+      description= "Predicting restaurants performance based on reviews",
+      author="Best team of Le Wagon #2195 batch",
+      install_requires=requirements,
+      packages=find_packages(),
+      include_package_data=True)
