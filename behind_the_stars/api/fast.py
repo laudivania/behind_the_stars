38bf1615e6f694a7ce931da6de302cf086882b28
Laudivania Ventura <laudivania.ventura@hotmail.com>
2026-03-16 12:53:30 +0100

    fast file updated

--- behind_the_stars/api/fast.py
+++ behind_the_stars/api/fast.py
@@ -20,30 +20,4 @@ def root():
     return {'greeting': 'Hello'}
 
 
-# http://127.0.0.1:8000/predict?pickup_datetime=2014-07-06+19:18:00&pickup_longitude=-73.950655&pickup_latitude=40.783282&dropoff_longitude=-73.984365&dropoff_latitude=40.769802&passenger_count=2
 #@app.get("/predict")
-# def predict(
-#         pickup_datetime: str,  # 2014-07-06 19:18:00
-#         pickup_longitude: float,    # -73.950655
-#         pickup_latitude: float,     # 40.783282
-#         dropoff_longitude: float,   # -73.984365
-#         dropoff_latitude: float,    # 40.769802
-#         passenger_count: int
-#     ):      # 1
-#     """
-#     Make a single course prediction.
-#     Assumes `pickup_datetime` is provided as a string by the user in "%Y-%m-%d %H:%M:%S" format
-#     Assumes `pickup_datetime` implicitly refers to the "US/Eastern" timezone (as any user in New York City would naturally write)
-#     """
-#     X_pred = pd.DataFrame(dict(
-#         pickup_datetime=[pd.Timestamp(pickup_datetime, tz='US/Eastern')],
-#         pickup_longitude=[pickup_longitude],
-#         pickup_latitude=[pickup_latitude],
-#         dropoff_longitude=[dropoff_longitude],
-#         dropoff_latitude=[dropoff_latitude],
-#         passenger_count=[passenger_count],))
-#     model = load_model()
-#     X_processed = preprocess_features(X_pred)
-#     y_pred = model.predict(X_processed)
-
-#     return {"fare" : float(y_pred[0][0])}
