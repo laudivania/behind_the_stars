5fbb1f630db3f3f99cfe364f1879d296648b99ac
Daniel Vidal Silvan <dvsilvan@hotmail.com>
2026-03-16 15:27:08 +0100

    params with env variables

--- behind_the_stars/params.py
+++ behind_the_stars/params.py
@@ -3,9 +3,11 @@ from dotenv import load_dotenv
 
 #GCP
 GCP_PROJECT = os.environ.get("GCP_PROJECT")
+GCP_REGION = os.environ.get("GCP_REGION")
 BUCKET_NAME = os.environ.get("BUCKET_NAME")
+MODEL_TARGET = os.environ.get("MODEL_TARGET")
 
 DATA_FILENAME = os.environ.get("DATA_FILENAME")
 LOCAL_DATA_PATH = os.environ.get("LOCAL_DATA_PATH")
-
+#LOCAL_REGISTRY_PATH = os.path.expanduser(os.environ.get("LOCAL_REGISTRY_PATH"))
 RANDOM_STATE = 42
