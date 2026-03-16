f9574674c6685e46b795422cd712a3599df5c01b
Gautier Vsx <@users.noreply.github.com>
2026-03-16 17:32:41 +0100

    working api

--- behind_the_stars/api/fast.py
+++ behind_the_stars/api/fast.py
@@ -1,11 +1,13 @@
 import pandas as pd
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from ml_logic.registry import load_dataset, load_model
+from behind_the_stars.ml_logic.registry import load_dataset, load_model
 # from behind_the_stars.ml_logic.preprocessor import preprocessing
 # from behind_the_stars.ml_logic.registry import load_model --> En attente du travail de Kenny-D
 
 app = FastAPI()
+df = load_dataset()
+model, vectorizer = load_model()
 
 # Allowing all middleware is optional, but good practice for dev purposes
 app.add_middleware(
@@ -28,18 +30,21 @@ def predict(
     """
     given a number of reviews (up to one hundred), predict the likelyhood of closure
     """
-    df = load_dataset()
-    model, vectorizer = load_model()
-    try :
+    if restaurant in df['name'].values:
         X_pred = df[df['name']==restaurant]['text'].tolist()
-    except :
-        print('no restaurant found')
-    X_vect = vectorizer(X_pred)
-    y_pred = model.predict(X_vect)
-    if y_pred==0:
-        y_proba = model.predict_proba(X_vect)[0]
-        proba = {'closure likelyhood' : float(y_proba)}
+
+        X_vect = vectorizer.transform(X_pred)
+
+        y_pred = model.predict(X_vect)
+
+        if y_pred[0]==0:
+            y_proba = model.predict_proba(X_vect)[0][0]
+            proba = {'closure likelyhood' : float(y_proba)}
+
+        else:
+            y_proba = model.predict_proba(X_vect)[0][1]
+            proba = {'open likelyhood' : float(y_proba)}
+
     else:
-        y_proba = model.predict_proba(X_vect)[1]
-        proba = {'open likelyhood' : float(y_proba)}
+        proba = {'open likelyhood' : 'could not answer, no restaurant found'}
     return proba
